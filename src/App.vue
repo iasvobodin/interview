5f1ff6f5d313a42854b95a74d0cb8310f9d4651d
iasvobodin <iasvobodin@gmail.com>
2021-07-21 00:09:54 +0500

    a few style

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="items">
-    <h1>Недвижимость</h1>
-    <p>Фильтр по типу</p>
-    <select v-model="filterType" name="" id="">
-       <option selected value="all">Все</option>
-      <option value="Жилое">Жилое</option>
-       <option value="Комерческое">Комерческое</option>
-    </select>
+    <div class="header">
+      <h1>Недвижимость</h1>
+      <div class="filter">
+        <p>Фильтр по типу</p>
+        <select v-model="filterType" name="" id="">
+          <option selected value="all">Все</option>
+          <option value="Жилое">Жилое</option>
+          <option value="Комерческое">Комерческое</option>
+        </select>
+      </div>
+    </div>
     <ul role="list" class="items__holder">
-      <li @click="changeItem(index)" class="list__item" v-for="(item, index) in items" :key="item.id">
+      <li
+        @click="changeItem(index)"
+        :class="{ blured: loading }"
+        class="list__item"
+        v-for="(item, index) in items"
+        :key="item.id"
+      >
         <div>Название: {{ item.name }}</div>
         <div>Описание: {{ item.desc }}</div>
         <div>Цена: ${{ item.price }}</div>
@@ -17,11 +27,22 @@
       </li>
     </ul>
     <div v-if="loading" class="preloader">
+      <div class="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
     <div class="pagination">
       <div class="numbering">
-        <span :class="{ exzact__pagination: currentPage === n }" class="pagination__numbers" @click="selectPage(n)"
-          v-for="n in pagesQty" :key="n">
+        <span
+          :class="{ exzact__pagination: currentPage === n }"
+          class="pagination__numbers"
+          @click="selectPage(n)"
+          v-for="n in pagesQty"
+          :key="n"
+        >
           {{ n }}
         </span>
       </div>
@@ -31,7 +52,6 @@
           <option v-for="n in [10, 20, 30]" :key="n">{{ n }}</option>
         </select>
       </div>
-
     </div>
     <div class="add__button">
       <button @click="addClearState">ADD NEW</button>
@@ -39,18 +59,34 @@
   </div>
   <div v-if="modal" class="modal">
     <div @mousedown="outsideClick($event)" class="modal__additem">
-      <form @submit.prevent="addItem" id="addform" class="modal__form" action="">
+      <form
+        @submit.prevent="addItem"
+        id="addform"
+        class="modal__form"
+        action=""
+      >
         <div class="input__holder">
           <h3>Названиe</h3>
           <input class="input" required v-model="vmodelItem.name" type="text" />
         </div>
         <div class="input__holder">
           <h3>Описание</h3>
-          <textarea required v-model="vmodelItem.desc" name="" cols="30" rows="5"></textarea>
+          <textarea
+            required
+            v-model="vmodelItem.desc"
+            name=""
+            cols="30"
+            rows="5"
+          ></textarea>
         </div>
         <div class="input__holder">
           <h3>Цена</h3>
-          <input class="input" required v-model="vmodelItem.price" type="number" />
+          <input
+            class="input"
+            required
+            v-model="vmodelItem.price"
+            type="number"
+          />
         </div>
         <div class="input__holder">
           <h3>Тип</h3>
@@ -59,12 +95,23 @@
             <option value="Жилое">Жилое</option>
           </select>
         </div>
-        <div class="input__button" >
-          <button @click.prevent="delItem" class="form__add" v-if="vmodelItem.state === 'Update'">Delete</button>
-          <input  class="form__add" :value="vmodelItem.state" type="submit" />
+        <div class="input__button">
+          <button
+            @click.prevent="delItem"
+            class="form__add"
+            v-if="vmodelItem.state === 'Update'"
+          >
+            Delete
+          </button>
+          <input class="form__add" :value="vmodelItem.state" type="submit" />
         </div>
         <div class="modal__close">
-          <img class="modal__close__icon" @click="modal = false" src="/img/cancel.svg" alt="" />
+          <img
+            class="modal__close__icon"
+            @click="modal = false"
+            src="/img/cancel.svg"
+            alt=""
+          />
           <!-- <button @click="modal = false">close</button> -->
         </div>
       </form>
@@ -78,25 +125,30 @@ import { useFetch } from "@/hooks/fetch";
 export default {
   setup() {
     const displayQty = ref(10);
-    const filterType = ref('all')
+    const filterType = ref("all");
     const state = reactive({
-      items: null,
+      items: 10,
       modal: false,
       maxIndex: null,
-      pagesQty: null,
+      addIndex: null,
+      pagesQty: 5,
       updateItem: {},
       currentPage: 1,
       vmodelItem: {},
-      loading: false
+      loading: false,
     });
     watch(displayQty, (newValue, oldValue) => {
       getItems(0, newValue, filterType.value);
       state.pagesQty = Math.floor(state.maxIndex / newValue) + 1;
       state.currentPage = 1;
     });
-        watch(filterType, (newValue, oldValue) => {
-          getCount()
-          getItems((state.currentPage - 1) * displayQty.value, displayQty.value, filterType.value);
+    watch(filterType, (newValue, oldValue) => {
+      getCount();
+      getItems(
+        (state.currentPage - 1) * displayQty.value,
+        displayQty.value,
+        filterType.value
+      );
     });
 
     const selectPage = (n) => {
@@ -105,24 +157,23 @@ export default {
     };
     const outsideClick = (e) => {
       const f = document.getElementById("addform"); // don't do it in vue!!! use costom directive
-      if(!f.contains(e.target)){
-        state.modal = false
-        
-      };
-
+      if (!f.contains(e.target)) {
+        state.modal = false;
+      }
     };
     //READ
-    const getCount = async ()=>{
-      const { request, response, loading} = useFetch(
+    const getCount = async () => {
+      const { request, response, loading } = useFetch(
         `/api/items?count=${filterType.value}`
       );
       await request();
-      state.maxIndex = response.value
-       state.pagesQty = Math.floor(state.maxIndex / displayQty.value) + 1;
-    } 
-    
+      state.maxIndex = response.value;
+      !state.addIndex && (state.addIndex = response.value);
+      state.pagesQty = Math.floor(state.maxIndex / displayQty.value) + 1;
+    };
+
     const getItems = async (offset, limit, fType) => {
-      const { request, response , loading} = useFetch(
+      const { request, response, loading } = useFetch(
         `/api/items?lim=${limit}&offset=${offset}&itemstype=${fType}`
       );
       state.loading = loading;
@@ -130,7 +181,7 @@ export default {
       state.items = response;
     };
     // INIT
-    getCount()
+    getCount();
     getItems(0, displayQty.value, filterType.value);
 
     //CREATE
@@ -143,7 +194,7 @@ export default {
       state.modal = true;
       //COPY OBJECT WITH SAVE STATE MABY IT HAS A BEST WAY
       state.vmodelItem = JSON.stringify(state.items[i]);
-      state.vmodelItem = JSON.parse(state.vmodelItem)
+      state.vmodelItem = JSON.parse(state.vmodelItem);
       state.vmodelItem.state = "Update";
       // COPY ID AND TYPE TO UPDATE AND DELETE
       state.updateItem.id = state.items[i].id;
@@ -151,10 +202,10 @@ export default {
     };
     const addItem = async () => {
       if (state.vmodelItem.state === "Add") {
-        const { request , loading} = useFetch("/api/items?add=true", {
+        const { request, loading } = useFetch("/api/items?add=true", {
           method: "POST",
           body: JSON.stringify({
-            index: ++state.maxIndex,
+            index: ++state.addIndex,
             type: state.vmodelItem["Тип"],
             ...state.vmodelItem,
           }),
@@ -163,7 +214,7 @@ export default {
       }
       if (state.vmodelItem.state === "Update") {
         const { id, type } = state.updateItem;
-        const { request , loading} = useFetch(
+        const { request, loading } = useFetch(
           `/api/items?update=true&type=${type}&id=${id}`,
           {
             method: "POST",
@@ -178,28 +229,41 @@ export default {
       state.modal = false;
       state.vmodelItem = {};
       //UPDATE LIST
-      getItems((state.currentPage - 1) * displayQty.value, displayQty.value, filterType.value);
+      getItems(
+        (state.currentPage - 1) * displayQty.value,
+        displayQty.value,
+        filterType.value
+      );
     };
     //UPDATE
 
     const updateItemDB = async (id, type) => {
-      const { request , loading} = useFetch(`/api/items?id=${id}&type=${type}`, {
-        method: "POST",
-        body: JSON.stringify({
-          ...state.vmodelItem,
-        }),
-      });
+      const { request, loading } = useFetch(
+        `/api/items?id=${id}&type=${type}`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            ...state.vmodelItem,
+          }),
+        }
+      );
       await request();
     };
     //DELETE
     const delItem = async () => {
       const { id, type } = state.updateItem;
-      const { request, loading} = useFetch(`/api/items?delete=true&id=${id}&type=${type}`); 
+      const { request, loading } = useFetch(
+        `/api/items?delete=true&id=${id}&type=${type}`
+      );
       state.loading = loading;
       await request();
-       state.modal = false;
-       state.vmodelItem = {};
-        getItems((state.currentPage - 1) * displayQty.value, displayQty.value, filterType.value);
+      state.modal = false;
+      state.vmodelItem = {};
+      getItems(
+        (state.currentPage - 1) * displayQty.value,
+        displayQty.value,
+        filterType.value
+      );
     };
     return {
       selectPage,
@@ -219,6 +283,10 @@ export default {
 </script>
 
 <style lang="css">
+:root {
+  --border__color: orange;
+}
+
 /* Box sizing rules */
 *,
 *::before,
@@ -280,7 +348,7 @@ textarea,
 select {
   font: inherit;
 }
-select{
+select {
   min-height: 30px;
 }
 h1 {
@@ -318,9 +386,11 @@ h1 {
   border-top: 1px solid black;
   border-right: 1px solid black;
   overflow: hidden;
+  transition: transform 0.5s;
 }
-.modal__close:hover{
-  border: 2px solid red;
+.modal__close:hover {
+  transform: scale(1.2);
+  transition: transform 0.5s;
 }
 .modal__additem {
   display: grid;
@@ -335,14 +405,14 @@ h1 {
   width: min(700px, 95vw);
   min-height: max(300px, 50vh);
   border-radius: 5px;
-  border: 1px solid black;
+  border: 1px solid var(--border__color);
   padding: 4vh 2vw;
   background-color: white;
 }
-.form__add{
-place-self: center end;
-min-height: 30px;
-min-width: 100px;
+.form__add {
+  place-self: center end;
+  min-height: 30px;
+  min-width: 100px;
 }
 .input__holder {
   width: 100%;
@@ -352,8 +422,8 @@ min-width: 100px;
   grid-template-columns: 1fr 4fr;
   margin: 0;
 }
-.input__button{
-width: 100%;
+.input__button {
+  width: 100%;
   margin: auto;
   display: grid;
   justify-content: space-evenly;
@@ -362,15 +432,15 @@ width: 100%;
   /* grid-template-columns: 1fr 4fr; */
   margin: 0;
 }
-.input__holder>h3{
-  place-self: center start
+.input__holder > h3 {
+  place-self: center start;
 }
 .list__item {
   min-height: 40px;
   margin-top: 3vh;
   margin-left: 3vw;
   margin-right: 3vw;
-  border: 1px solid orange;
+  border: 1px solid var(--border__color);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 5px;
   padding: 1vh;
@@ -381,13 +451,16 @@ width: 100%;
 } */
 @media (max-width: 450px) {
   .input__holder {
- display: grid;
- grid-auto-flow: row;
-  grid-template-columns: 1fr;
-  /* grid-template-rows: 1fr 1fr; */
+    display: grid;
+    grid-auto-flow: row;
+    grid-template-columns: 1fr;
+    /* grid-template-rows: 1fr 1fr; */
+  }
+  .header > h1 {
+    text-align: start;
   }
 }
-.pagination{
+.pagination {
   position: fixed;
   bottom: 0;
   width: 100%;
@@ -405,23 +478,101 @@ width: 100%;
   cursor: pointer;
 }
 .pagination__numbers:hover {
-  border: 1px solid orange;
+  border: 1px solid var(--border__color);
 }
 .exzact__pagination {
-  background-color: orange;
+  background-color: var(--border__color);
 }
-.add__button{
+.add__button {
   position: fixed;
   bottom: 20px;
   right: 20px;
-  
 }
-.preloader{
+.preloader {
   height: 100vh;
   width: 100%;
-  background-color: rgba(255, 166, 0, 0.226);
+  display: grid;
+  background-color: rgba(255, 255, 255, 0.8);
   position: fixed;
   top: 0;
   left: 0;
+}
+.blured {
+  filter: blur(3px);
+}
+.filter {
+  position: absolute;
+  right: 5px;
+}
+.header {
+  z-index: 2;
+  position: sticky;
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0px 5px 5px 0px #0000003b;
+  border: 1px solid var(--border__color);
+  top: 0px;
+  margin: 1vh;
+  padding: 5px;
+  border-radius: 5px;
+  display: grid;
+  grid-auto-flow: column;
+}
+/* PRELOADER */
+
+.lds-ellipsis {
+  place-self: center;
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ellipsis div {
+  position: absolute;
+  top: 33px;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background: orange;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.lds-ellipsis div:nth-child(1) {
+  left: 8px;
+  animation: lds-ellipsis1 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(2) {
+  left: 8px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(3) {
+  left: 32px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(4) {
+  left: 56px;
+  animation: lds-ellipsis3 0.6s infinite;
+}
+@keyframes lds-ellipsis1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes lds-ellipsis3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+@keyframes lds-ellipsis2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(24px, 0);
+  }
 }
 </style>
